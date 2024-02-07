5869f3bbc3e4b3e6db157dabcfc89029ed9b9554
Rudher <rudrabhardwaj121201@gmail.com>
2024-02-07 22:27:57 +0530

    assignments

--- 07-02-2024/assignment-3/index.js
+++ 07-02-2024/assignment-3/index.js
@@ -3,50 +3,56 @@ function numberGuessing() {
   let randomNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 
   let count = 0;
-  let run = true
+  let run = true;
+
+  const history_score = [];
+
   while (run) {
     let guess;
 
     guess = parseInt(prompt("Guess the number between 1-100: "));
     console.log(guess);
 
-    if (guess < 1 || guess > 100) {
+    if (guess < 1 || guess > 100 || isNaN(guess)) {
       alert("Enter the valid guess.");
-    } else if (isNaN(guess)) {
-      window.alert("Invalid! Enter number again");
-    } else {
-      count++;
-      if (guess < randomNumber) {
-        window.alert("Too low");
-      } else if (guess > randomNumber) {
-        window.alert("Too high");
-      }
-      else {
-        window.alert(`Correct. It took you ${count} attempts.`);
-        calculateScore(count);
-        break;
-
-      }
     }
+    count = count + 1;
+
+    history_score.push(guess);
+
+    if (guess < randomNumber) {
+      window.alert("Too low");
+    } else if (guess > randomNumber) {
+      window.alert("Too high");
+    }
+    else {
+      window.alert(`Correct. It took you ${count} attempts.`);
+      score(count);
+      showHistory(history_score);
+      break;
+
+    }
+
   }
   let again = confirm("Want to play again?");
   return again;
 }
-
-function calculateScore(count) {
-  let score = 0;
-
-  if (count <= 6) {
-    score = 10;
+function showHistory(history) {
+  alert(`history: ${history.join(', ')}`);
+}
+function score(count) {
+  let claculatedScore = 0;
+  if (count <= 5) {
+    claculatedScore = 12;
   } else if (count <= 10) {
-    score = 8;
-  } else if (count <= 13) {
-    score = 5;
+    claculatedScore = 8;
+  } else if (count <= 14) {
+    claculatedScore = 4
   } else {
-    score = 2;
+    claculatedScore = 2;
   }
-  alert(`Your score is: ${score}`);
 
+  alert(`Your score: ${claculatedScore}`);
 }
 
 function main() {
