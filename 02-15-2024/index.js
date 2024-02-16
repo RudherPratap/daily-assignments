2e3ecc549651107fe67cc2da5fbbaf357824067a
Rudher <rudrabhardwaj121201@gmail.com>
2024-02-16 12:02:45 +0530

    assingnments

--- 02-15-2024/index.js
+++ 02-15-2024/index.js
@@ -1,3 +1,5 @@
+import { renderData } from "./ui.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   const render = document.getElementById("render");
 
@@ -25,33 +27,3 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Error in fetching the posts: ", err);
     });
 });
-function renderData(users, posts, todos) {
-  const render = document.getElementById("render");
-
-  // Render users
-  const usersList = document.createElement("ul");
-  users.forEach(user => {
-    const li = document.createElement("li");
-    li.textContent = user.name;
-    usersList.appendChild(li);
-  });
-  render.appendChild(usersList);
-
-  // Render posts
-  const postsList = document.createElement("ul");
-  posts.forEach(post => {
-    const li = document.createElement("li");
-    li.textContent = post.title;
-    postsList.appendChild(li);
-  });
-  render.appendChild(postsList);
-
-  // Render todos
-  const todosList = document.createElement("ul");
-  todos.forEach(todo => {
-    const li = document.createElement("li");
-    li.textContent = todo.title;
-    todosList.appendChild(li);
-  });
-  render.appendChild(todosList);
-}
