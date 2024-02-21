49b3161984f1263b6e9644f644e1a36f586fe357
Rudher <rudrabhardwaj121201@gmail.com>
2024-02-21 11:26:36 +0530

    assignments

--- 21-02-2024/node/index.js
+++ 21-02-2024/node/index.js
@@ -1,17 +1,12 @@
 const fileSystemModule = require('./fileSystemModule');
 
-// Demonstrate the usage of each function
 (async () => {
-  // List files in a specific directory
   await fileSystemModule.listFiles("21-02-2024");
 
-  // Create a new directory
   await fileSystemModule.createDirectory('21-02-2024/node/newDirectory');
 
-  // Read the content of a text file
   await fileSystemModule.readFileContent('21-02-2024/node/newDirectory/newFile.txt');
 
-  // Write new content to a text file
   await fileSystemModule.writeToFile('21-02-2024/node/newDirectory/newFile.txt', "This is a new file content.");
 
 })();
