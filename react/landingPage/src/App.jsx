40442a2e1fe10363031cb96b13170b7a9e7870cd
Rudher <rudrabhardwaj121201@gmail.com>
2024-03-12 23:49:21 +0530

    assignments-react

---
+++ react/landingPage/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent';
+import './style/styles.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ react/landingPage/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2024 Product Landing Page</p>
+    </footer>
+  );
+};
+
+export default Footer;
